14061bf66d980dcbd91ea9c9d0c53666e3bf764a
EnricGDV <45202083+EnricGDV@users.noreply.github.com>
2021-09-27 10:23:10 +0200

    Threads & Coroutines

---
+++ Threads Project/Assets/SecondPrinterB.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class SecondPrinterB : MonoBehaviour
+{
+
+    private bool exit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Thread myThread = new Thread(threadInfiniteSecondPrinter);
+        myThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            exit = true;
+        }
+    }
+
+
+
+    void threadInfiniteSecondPrinter()
+    {
+        Debug.LogWarning("Starting Thread!!");
+        DateTime myTime;
+
+        while (!exit)
+        {
+            myTime = System.DateTime.UtcNow;
+            while ((System.DateTime.UtcNow - myTime).Seconds < 5f)
+            {
+            }
+            Debug.Log("5 more seconds have passed!");
+        }
+        Debug.LogWarning("Thread Stopping!!");
+    }
+}
