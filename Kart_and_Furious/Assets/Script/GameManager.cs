a46d7030787b6edce0d0b4ac1c9a264637c0b181
EnricGDV <minenime4thewin@gmail.com>
2021-12-19 20:56:22 +0100

    Game Logic (Not Tested)

--- Kart_and_Furious/Assets/Script/GameManager.cs
+++ Kart_and_Furious/Assets/Script/GameManager.cs
@@ -7,11 +7,23 @@ public class GameManager : MonoBehaviour
     public GameObject kartPrefab;
     public Transform initialCoords;
     public GameObject camera;
+    public int maxLaps = 1;
+    private int laps = 0;
 
     private void Start()
     {
         AddKart(1);
     }
+
+    private void Update()
+    {
+        if(laps == maxLaps+1)
+        {
+            Debug.Log("Race Finished!!");
+        }
+    }
+
+
     public void AddKart(int id)
     {
         GameObject newPlayer = GameObject.Find("Client");
@@ -19,6 +31,10 @@ public class GameManager : MonoBehaviour
         newKart.name = "Kart " + id;
         newKart.transform.position = new Vector3(initialCoords.position.x - 0.8f * id, initialCoords.position.y, initialCoords.position.z - 0.6f * (id%2-1));
         camera.GetComponent<CameraScript>().target = newKart;
+    }
 
+    public void AddLap()
+    {
+        laps++;
     }
 }
---
+++ Kart_and_Furious/Assets/Script/GoalScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public GameObject manager;
+    private int laps1 = 0;
+    private int laps2 = 0;
+    private int laps3 = 0;
+    private int laps4 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Kart1")
+        {
+            laps1++;
+            if (laps2 < laps1 && laps3 < laps1 && laps4 < laps1)
+                manager.GetComponent<GameManager>().AddLap();
+        }
+        else if (other.gameObject.name == "Kart2")
+        {
+            laps2++;
+            if (laps1 < laps2 && laps3 < laps2 && laps4 < laps2)
+                manager.GetComponent<GameManager>().AddLap();
+        }
+        else if (other.gameObject.name == "Kart3")
+        {
+            laps3++;
+            if (laps1 < laps3 && laps2 < laps3 && laps4 < laps3)
+                manager.GetComponent<GameManager>().AddLap();
+        }
+        else if (other.gameObject.name == "Kart4")
+        {
+            laps4++;
+            if (laps1 < laps4 && laps2 < laps4 && laps3 < laps4)
+                manager.GetComponent<GameManager>().AddLap();
+        }
+    }
+}
